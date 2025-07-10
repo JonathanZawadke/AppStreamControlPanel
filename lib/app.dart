28e508e6608924dbc6da96055717e01bef8e5d6c
zawadke <jzawadke@gmail.com>
2025-07-10 10:36:09 +0200

    refactor: split main.dart and move MyApp to app.dart

---
+++ lib/app.dart
@@ -0,0 +1,34 @@
+import 'package:appstreamcontrolpanel/classes/language_change_provider.dart';
+import 'package:appstreamcontrolpanel/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:appstreamcontrolpanel/pages/home_page.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var languageProvider = Provider.of<LanguageChangeProvider>(context);
+
+    return MaterialApp(
+      title: 'AppStreamControlPanel',
+      debugShowCheckedModeBanner: false,
+      locale: languageProvider.currentLocale,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale('en'), Locale('de')],
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: BLUE),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,10 @@
+import 'package:appstreamcontrolpanel/app.dart';
 import 'package:appstreamcontrolpanel/classes/language_change_provider.dart';
-import 'package:appstreamcontrolpanel/constant.dart';
 import 'package:appstreamcontrolpanel/global_variable.dart';
 import 'package:flutter/material.dart';
-import 'package:appstreamcontrolpanel/pages/home_page.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:window_manager/window_manager.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'dart:io' show Platform;
 
 void main() async {
@@ -42,31 +39,4 @@ void main() async {
       child: const MyApp(),
     ),
   );
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    var languageProvider = Provider.of<LanguageChangeProvider>(context);
-
-    return MaterialApp(
-      title: 'AppStreamControlPanel',
-      debugShowCheckedModeBanner: false,
-      locale: languageProvider.currentLocale,
-      localizationsDelegates: const [
-        AppLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [Locale('en'), Locale('de')],
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: BLUE),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
 }
